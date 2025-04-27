c8311902bc80c845e2658c6d2ac3e879342d093b
Pearce Raegler <pdr2@pct.edu>
2025-04-27 17:07:04 -0400

    menu

--- Source/UnrealProject/UnrealProjectGameInstance.cpp
+++ Source/UnrealProject/UnrealProjectGameInstance.cpp
@@ -11,7 +11,7 @@ void UUnrealProjectGameInstance::LeaveGame()
 	UGameplayStatics::OpenLevel(world, FName(TEXT("/Game/FirstPerson/Maps/MenuMap.MenuMap")));
 }
 
-void UUnrealProjectGameInstance::LeaveGame()
+void UUnrealProjectGameInstance::StartGame()
 {
 	auto world = GEngine->GetCurrentPlayWorld();
 	UGameplayStatics::OpenLevel(world, FName(TEXT("/Game/FirstPerson/Maps/GameMap.GameMap")));
